3435f9b46b40efc0bafe9d2991fe630a4e110e2c
govindongit <govindnag5863@gmail.com>
2025-01-15 18:45:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import WordLetterCounter from './Component/WordLetterCounter/WordLetterCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Word - Letter Counter</h2>
+      </div>
+      <WordLetterCounter/>
+    </div>
+  );
+}
+
+export default App;
